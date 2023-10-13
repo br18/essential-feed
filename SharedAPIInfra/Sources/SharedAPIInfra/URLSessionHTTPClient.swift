5cf701f9f34b1bd88a94087652c37d5afa90c4b1
Ben Rosen <benrosen@zap-map.com>
2023-10-13 15:18:59 +0100

    Create empty API infra package which will contain the API infrastructure (URLSession Implementation)

---
+++ SharedAPIInfra/Sources/SharedAPIInfra/URLSessionHTTPClient.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ben Rosen on 13/10/2023.
+//
+
+import Foundation
---
+++ SharedAPIInfra/Tests/SharedAPIInfraTests/URLSessionHTTPClientTests.swift
@@ -0,0 +1,6 @@
+import XCTest
+@testable import SharedAPIInfra
+
+final class URLSessionHTTPClientTest: XCTestCase {
+    
+}
