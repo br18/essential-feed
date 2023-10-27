d979c44916c0f6b864dc728ffb070241331fa08b
Ben Rosen <benrosen@zap-map.com>
2023-10-27 16:02:44 +0100

    Create feed loader and initial use case test

---
+++ FeedCache/Sources/FeedCache/LocalFeedLoader.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ben Rosen on 27/10/2023.
+//
+
+import Foundation
+
+public final class LocalFeedLoader {
+
+    public init(store: FeedStore, currentDate: @escaping () -> Date) {
+    }
+}
