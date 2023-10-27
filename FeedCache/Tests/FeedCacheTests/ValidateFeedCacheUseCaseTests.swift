73da8ce6a278f1c53996b815626bc28925259808
br18 <63476046+br18@users.noreply.github.com>
2023-10-27 20:48:16 +0100

    Add valid init cache tests

--- FeedCache/Tests/FeedCacheTests/ValidateFeedCacheUseCaseTests.swift
+++ FeedCache/Tests/FeedCacheTests/ValidateFeedCacheUseCaseTests.swift
@@ -1,1 +1,23 @@
 import Foundation
+import XCTest
+import FeedFeature
+import FeedCache
+
+class ValidateFeedCacheUseCaseTests: XCTestCase {
+
+    func test_init_doesNotMessageStoreUponCreation() {
+        let (_, store) = makeSUT()
+
+        XCTAssertEqual(store.receivedMessages, [])
+    }
+
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
+        let store = FeedStoreSpy()
+        let sut = LocalFeedLoader(store: store, currentDate: currentDate)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
+    }
+
+
+}
