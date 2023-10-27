40b52deab1cf7aaac05ac0fba78646a8079fc372
Ben Rosen <benrosen@zap-map.com>
2023-10-27 14:10:38 +0100

    Add checks for bad responses and return empty array for now if successful

--- FeedAPI/Sources/FeedAPI/FeedItemsMapper.swift
+++ FeedAPI/Sources/FeedAPI/FeedItemsMapper.swift
@@ -2,12 +2,26 @@ import Foundation
 import FeedFeature
 
 public final class FeedItemsMapper {
+    private struct Root: Decodable {
+        private let items: [RemoteFeedItem]
+
+        private struct RemoteFeedItem: Decodable {
+            let id: UUID
+            let description: String?
+            let location: String?
+            let image: URL
+        }
+    }
 
     public enum Error: Swift.Error {
         case invalidData
     }
 
     public static func map(_ data: Data, from response: HTTPURLResponse) throws -> [FeedImage] {
-        throw Error.invalidData
+        guard response.isOK, (try? JSONDecoder().decode(Root.self, from: data)) != nil else {
+            throw Error.invalidData
+        }
+
+        return []
     }
 }
