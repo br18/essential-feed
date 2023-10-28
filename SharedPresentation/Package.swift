ce905de270a48dd927c473db60b15ffb05d2c65e
br18 <63476046+br18@users.noreply.github.com>
2023-10-28 16:41:30 +0100

    Add shared presentation package

--- SharedPresentation/Package.swift
+++ SharedPresentation/Package.swift
@@ -4,25 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "SharedUI",
-    platforms: [.iOS(.v17)],
+    name: "SharedPresentation",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "SharedUI",
-            targets: ["SharedUI"]),
-    ],
-    dependencies: [
-          .package(name: "SharedPresentation", path: "../SharedPresentation")
+            name: "SharedPresentation",
+            targets: ["SharedPresentation"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SharedUI",
-            dependencies: ["SharedPresentation"]),
+            name: "SharedPresentation"),
         .testTarget(
-            name: "SharedUITests",
-            dependencies: ["SharedUI"]),
+            name: "SharedPresentationTests",
+            dependencies: ["SharedPresentation"]),
     ]
 )
