b7bfbec52d1f615b7f1b71fc4cd5001dd5890ac8
br18 <63476046+br18@users.noreply.github.com>
2023-11-09 19:51:00 +0000

    Add saving functionality, ignore errors for now

--- FeedCache/Sources/FeedCache/LocalFeedImageDataLoader.swift
+++ FeedCache/Sources/FeedCache/LocalFeedImageDataLoader.swift
@@ -1,9 +1,16 @@
 import Foundation
+import FeedFeature
 
 public final class LocalFeedImageDataLoader {
     private let store: FeedImageDataStore
 
     public init(store: FeedImageDataStore) {
         self.store = store
+    }
+}
+
+extension LocalFeedImageDataLoader: FeedImageDataCache {
+    public func save(_ data: Data, for url: URL) throws {
+        try? store.insert(data, for: url)
     }
 }
--- FeedCache/Tests/FeedCacheTests/LoadFeedImageDataFromCacheUseCaseTests.swift
+++ FeedCache/Tests/FeedCacheTests/LoadFeedImageDataFromCacheUseCaseTests.swift
@@ -1,6 +1,7 @@
 import Foundation
 import FeedCache
 import XCTest
+import SharedTestHelpers
 
 class LoadFeedImageDataFromCacheUseCaseTests: XCTestCase {
 
@@ -9,7 +10,17 @@ class LoadFeedImageDataFromCacheUseCaseTests: XCTestCase {
 
         XCTAssertTrue(store.receivedMessages.isEmpty)
     }
-    
+
+    func test_saveImageDataForURL_requestsImageDataInsertionForURL() {
+        let (sut, store) = makeSUT()
+        let url = anyURL()
+        let data = anyData()
+
+        try? sut.save(data, for: url)
+
+        XCTAssertEqual(store.receivedMessages, [.insert(data: data, for: url)])
+    }
+
     // MARK: - Helpers
 
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedImageDataLoader, store: FeedImageDataStoreSpy) {
