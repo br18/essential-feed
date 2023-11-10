9b93731865f385e29ce5c62f207ca3aa7324b064
br18 <63476046+br18@users.noreply.github.com>
2023-11-10 22:24:18 +0000

    Test init does not save anything to the store

---
+++ FeedCache/Tests/FeedCacheTests/LoadFeedImageDataUseCaseTests.swift
@@ -0,0 +1,20 @@
+import XCTest
+import SharedTestHelpers
+import FeedCache
+
+class LoadFeedImageDataFromCacheUseCaseTests: XCTestCase {
+
+    func test_init_doesNotMessageStoreUponCreation() {
+        let (_, store) = makeSUT()
+
+        XCTAssertTrue(store.receivedMessages.isEmpty)
+    }
+
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedImageDataLoader, store: FeedImageDataStoreSpy) {
+        let store = FeedImageDataStoreSpy()
+        let sut = LocalFeedImageDataLoader(store: store)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
+    }
+}
