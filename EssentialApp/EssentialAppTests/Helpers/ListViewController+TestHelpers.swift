e20f08baff291b138dd485e9902cb773093121dd
br18 <63476046+br18@users.noreply.github.com>
2023-11-12 17:55:41 +0000

    Request image when redisplaying the cell

--- EssentialApp/EssentialAppTests/Helpers/ListViewController+TestHelpers.swift
+++ EssentialApp/EssentialAppTests/Helpers/ListViewController+TestHelpers.swift
@@ -102,6 +102,17 @@ extension ListViewController {
         return view
     }
 
+    @discardableResult
+    func simulateFeedImageBecomingVisibleAgain(at row: Int) -> FeedImageCell? {
+        let view = simulateFeedImageViewNotVisible(at: row)
+
+        let delegate = tableView.delegate
+        let index = IndexPath(row: row, section: feedImagesSection)
+        delegate?.tableView?(tableView, willDisplay: view!, forRowAt: index)
+
+        return view
+    }
+
     func simulateTapOnFeedImage(at row: Int) {
         let delegate = tableView.delegate
         let index = IndexPath(row: row, section: feedImagesSection)
