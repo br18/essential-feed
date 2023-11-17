a9d2e28126977ce6ea90085cd053ac86319aa78f
br18 <63476046+br18@users.noreply.github.com>
2023-11-17 10:59:22 +0000

    Add a test that still passes when just throwing an error

--- ImageCommentsAPI/Tests/ImageCommentsAPITests/ImageCommentsMapperTests.swift
+++ ImageCommentsAPI/Tests/ImageCommentsAPITests/ImageCommentsMapperTests.swift
@@ -13,4 +13,15 @@ final class ImageCommentsAPITests: XCTestCase {
             )
         }
     }
+
+    func test_map_throwsErrorOn2xxHTTPResponseWithInvalidJSON() throws {
+        let invalidJSON = Data("invalid json".utf8)
+        let samples = [200, 201, 250, 280, 299]
+
+        try samples.forEach { code in
+            XCTAssertThrowsError(
+                try ImageCommentsMapper.map(invalidJSON, from: HTTPURLResponse(statusCode: code))
+            )
+        }
+    }
 }
