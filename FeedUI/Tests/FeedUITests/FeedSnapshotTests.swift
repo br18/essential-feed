5ca2b70f1bbec282b28165740da0b031769bfc8e
br18 <63476046+br18@users.noreply.github.com>
2023-11-19 15:12:52 +0000

    Create a loading more indicator for a feed

--- FeedUI/Tests/FeedUITests/FeedSnapshotTests.swift
+++ FeedUI/Tests/FeedUITests/FeedSnapshotTests.swift
@@ -26,6 +26,15 @@ class FeedSnapshotTests: XCTestCase {
         assert(snapshot: sut.snapshot(for: .iPhone(style: .dark)), named: "FEED_WITH_FAILED_IMAGE_LOADING_dark")
     }
 
+    func test_feedWithLoadMoreIndicator() {
+        let sut = makeSUT()
+
+        sut.display(feedWithLoadMoreIndicator())
+
+        assert(snapshot: sut.snapshot(for: .iPhone(style: .light)), named: "FEED_WITH_LOAD_MORE_INDICATOR_light")
+        assert(snapshot: sut.snapshot(for: .iPhone(style: .dark)), named: "FEED_WITH_LOAD_MORE_INDICATOR_dark")
+    }
+
     // MARK: - Helpers
 
     private func makeSUT() -> ListViewController {
@@ -65,6 +74,23 @@ class FeedSnapshotTests: XCTestCase {
                 location: "Brighton Seafront",
                 image: nil
             )
+        ]
+    }
+
+    private func feedWithLoadMoreIndicator() -> [CellController] {
+        let loadMore = LoadMoreCellController()
+        loadMore.display(ResourceLoadingViewModel(isLoading: true))
+        return feedWith(loadMore: loadMore)
+    }
+
+    private func feedWith(loadMore: LoadMoreCellController) -> [CellController] {
+        let stub = feedWithContent().last!
+        let cellController = FeedImageCellController(viewModel: stub.viewModel, delegate: stub, selection: {})
+        stub.controller = cellController
+
+        return [
+            CellController(id: UUID(), cellController),
+            CellController(id: UUID(), loadMore)
         ]
     }
 
