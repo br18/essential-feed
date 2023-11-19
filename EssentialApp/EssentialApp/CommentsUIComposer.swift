1953b6b6627c22594c91cafcf63b1f9884eb5bfd
br18 <63476046+br18@users.noreply.github.com>
2023-11-19 11:50:30 +0000

    Use comments view controller to add the title to

--- EssentialApp/EssentialApp/CommentsUIComposer.swift
+++ EssentialApp/EssentialApp/CommentsUIComposer.swift
@@ -1,8 +1,10 @@
 import Foundation
+import UIKit
 import SharedUI
 import ImageCommentsFeature
 import Combine
 import ImageCommentsPresentation
+import ImageCommentsUI
 
 public final class CommentsUIComposer {
     private init() {}
@@ -10,8 +12,14 @@ public final class CommentsUIComposer {
     public static func commentsComposedWith(
         commentsLoader: @escaping () -> AnyPublisher<[ImageComment], Error>
     ) -> ListViewController {
-        let viewController = ListViewController()
-        viewController.title = ImageCommentsPresenter.title
-        return viewController
+        return makeCommentsViewController(title: ImageCommentsPresenter.title)
+    }
+
+    private static func makeCommentsViewController(title: String) -> ListViewController {
+        let bundle = ImageCommentsUI.bundle
+        let storyboard = UIStoryboard(name: "ImageComments", bundle: bundle)
+        let controller = storyboard.instantiateInitialViewController() as! ListViewController
+        controller.title = title
+        return controller
     }
 }
